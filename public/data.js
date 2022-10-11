a4b387db5425869cfa0c273237f5b18dd47b6ce1
saoussenn <saoussenad90@gmail.com>
2022-10-11 13:03:19 +0200

    first commit

---
+++ public/data.js
@@ -0,0 +1,9 @@
+export const datalist = [
+    {
+      title: "Mulan",
+      description: "Critics Consensus: Exploring themes of family duty and honor, Mulan breaks new ground as a Disney film, while still bringing vibrant animation and sprightly characters to the screen.",
+      img: "https://resizing.flixster.com/n-8JUu78RqW8sAwUggmdNJoDr4U=/fit-in/180x240/v2/https://flxt.tmsimg.com/assets/p21118_p_v8_ac.jpg",
+      rate: "85%",
+      trailer:"https://www.youtube.com/watch?v=HKH7_n425Ss" 
+    },
+    {
---
+++ public/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import movieSlice from './slices/movieSlice'
+
+
+export const store = configureStore({
+  reducer: {
+    movie: movieSlice, 
+  },
+})
